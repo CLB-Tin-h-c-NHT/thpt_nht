93964396b2051678864da9d9e2dd2fe97abd1627
Sang464 <phams1631@gmail.com>
2023-04-14 17:49:57 +0700

    sang commit

---
+++ assets/scripts/slider.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function(){
+    const slider = document.querySelector(".slider");
+    const sliderMain = document.querySelector(".slider-main")
+    const sliderItem = document.querySelectorAll(".slider-item")
+    const sliderItemWidth = sliderItem[0].offsetWidth;
+    const sliderLength = sliderItem.length;
+    console.log(sliderLength);
+    let positionX = 0;
+    let id = 0;
+
+    setInterval(function(){
+        id += 1;
+        if (id == sliderLength){
+            id = 0;
+            sliderMain.classList.remove('active');
+        } else sliderMain.classList.add('active');
+        positionX = -(sliderItemWidth*id);
+        sliderMain.style = `transform: translateX(${positionX}px)`;
+    }, 5000)
+})
