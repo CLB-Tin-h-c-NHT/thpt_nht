645a594c97eef7d99f8fa7ce0bf0dadcc4a26b17
Sang464 <phams1631@gmail.com>
2023-05-20 09:19:25 +0700

    Sang

--- models/chatGPT.js
+++ models/chatGPT.js
@@ -14,15 +14,24 @@ const openai = new OpenAIApi(configuration)
 router.post('/chatGPT', async (req, res, next)=>{
     try{
         const prompt = req.body.prompt
+        // const response = await openai.createCompletion({
+        //     model: "text-davinci-003",
+        //     prompt: prompt,
+        //     temperature: 1,
+        //     max_tokens: 3000,
+        //     top_p: 1,
+        //     frequency_penalty: 0,
+        //     presence_penalty: 0,
+        //   });
         const response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [{role: "user", content: prompt}]
         })
         res.send({
             message: response.data.choices[0].message.content
+            // message: response.data.choices[0].text
         })
     } catch(err){
-        console.log(err)
         res.status(500).send("Error server")
     }
 })
